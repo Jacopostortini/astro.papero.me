76faf42aaf0a8ae84c51281162b4d79fe3504f96
Jacopostortini <jacostortini@gmail.com>
2021-03-18 19:47:36 +0100

    J

---
+++ src/MainScene.js
@@ -0,0 +1,33 @@
+import * as Phaser from "phaser";
+import io from "socket.io-client";
+
+export default class MainScene extends Phaser.Scene {
+
+    constructor() {
+        super({key: "Main"});
+        this.socket = io("https://papero.me/server/astro", {autoConnect: true});
+        this.socket.on("connect", ()=>{
+            console.log("connected");
+        });
+    }
+
+    preload(){
+        this.load.image("ship", "./asteroids_ship.png");
+    }
+
+    create(){
+        this.ship = this.physics.add.image(400, 300, "ship");
+        this.ship.setCollideWorldBounds(true);
+        this.keyA = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.A);
+        this.socket.on("move", ()=>{
+           this.ship.rotation += Math.PI/90;
+        });
+    }
+
+    update(){
+        if(this.keyA.isDown) {
+            this.ship.rotation += Math.PI/90;
+            this.socket.emit("move");
+        }
+    }
+}
