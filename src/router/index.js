958f5f7a42c4e6782b89f140a2dc6db555c3f57d
Jacopostortini <jacostortini@gmail.com>
2021-03-18 20:51:44 +0100

    J

--- src/router/index.js
+++ src/router/index.js
@@ -4,7 +4,7 @@ const Home = () => import("../views/HelloWorld" /* webpackChunkName: "home" */);
 
 const routes = [
     {
-        path: '/',
+        path: '/astro',
         name: 'Home',
         component: Home
     }
--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    publicPath: "/",
+    publicPath: "/astro/",
     /*css: {
         loaderOptions: {
             sass: {
