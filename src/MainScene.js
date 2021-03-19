7e42a65815ccde523b735af5a9539463c66b9410
Jacopostortini <jacostortini@gmail.com>
2021-03-19 11:39:43 +0100

    J

--- src/MainScene.js
+++ src/MainScene.js
@@ -5,7 +5,7 @@ export default class MainScene extends Phaser.Scene {
 
     constructor() {
         super({key: "Main"});
-        this.angularVelocity = Math.PI*3;
+        this.angularVelocity = Math.PI*2;
         this.velocity = 200;
         this.socket = io("https://papero.me", {
             path: "/server/astro/socket.io",
@@ -17,31 +17,65 @@ export default class MainScene extends Phaser.Scene {
     }
 
     preload(){
-        this.load.image("ship", "./asteroids_ship.png");
+        this.load.image("ship", "http://labs.phaser.io/assets/sprites/asteroids_ship.png");
+        this.socket.emit("get-game", (data)=>{
+           this.players = [];
+           for(let i= 0; i < data.players.length; i++){
+               this.players.push({
+                   id: data.players[i].id,
+                   position: data.players[i].position,
+                   rotation: data.players[i].rotation
+               });
+           }
+            this.socket.emit("new-player");
+        });
     }
 
     create(){
-        this.ship = this.physics.add.image(400, 300, "ship");
-        this.ship.setCollideWorldBounds(true);
+        this.players.forEach((player)=>{
+            player.ship = this.physics.add.image(player.position.x, player.position.y, "ship");
+            player.ship.rotation = player.rotation;
+            player.ship.setCollideWorldBounds(true);
+        })
         this.keyA = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.A);
         this.socket.on("move", (data)=>{
-           this.ship.rotation = data.rotation;
-           this.ship.x = data.position.x;
-            this.ship.y = data.position.y;
+           this.players.forEach(player => {
+               if(player.id === data.id){
+                   player.ship.rotation = data.rotation;
+                   player.ship.x  = data.position.x;
+                   player.ship.y = data.position.y;
+               }
+           });
+        });
+        this.socket.on("new-player", (id)=> {
+            this.playerID = id;
+            this.players.push({
+                id: id,
+                position: {
+                    x: 300,
+                    y: 400
+                },
+                rotation: 0
+            });
         });
     }
 
     update(time, delta){
-        if(this.keyA.isDown) {
-            this.ship.rotation += this.angularVelocity*delta/1000;
-            this.socket.emit("move", {
-                rotation: this.ship.rotation,
-                position: {
-                    x: this.ship.x,
-                    y: this.ship.y
+        this.players.forEach(player => {
+            if(player.id === this.playerID){
+                if(this.keyA.isDown){
+                    player.ship.rotation += this.angularVelocity*delta/1000;
+                    this.socket.emit("move", {
+                        player: this.playerID,
+                        rotation: player.ship.rotation,
+                        position: {
+                            x: player.ship.x,
+                            y: player.ship.y
+                        }
+                    });
                 }
-            });
-        }
-        this.ship.setVelocity(this.velocity*Math.cos(this.ship.rotation), this.velocity*Math.sin(this.ship.rotation));
+            }
+            player.ship.setVelocity(this.velocity*Math.cos(player.ship.rotation), player.velocity*Math.sin(player.ship.rotation));
+        });
     }
 }
