f7b48d0d860e18585a8e0b73b29fa2646de68558
Jacopostortini <jacostortini@gmail.com>
2021-03-31 15:11:52 +0200

    J

--- src/phaser/GameScene.js
+++ src/phaser/GameScene.js
@@ -167,7 +167,8 @@ export default class GameScene extends Phaser.Scene {
         if(deltaTime<=0) return;
 
         player.ship.setVelocity( ( data[2][0]-player.ship.x ) / deltaTime, ( data[2][1]-player.ship.y ) / deltaTime );
-        player.ship.setAngularVelocity( 180 / Math.PI * (data[1]-player.ship.rotation) / deltaTime );
+        const deltaTheta = ( data[1]-player.ship.rotation>0 ) ? data[1]-player.ship.rotation>0 : Math.PI - data[1]-player.ship.rotation;
+        player.ship.setAngularVelocity( 180 / Math.PI * deltaTheta / deltaTime );
 
         player.lastTimestamp = data[3];
     }
