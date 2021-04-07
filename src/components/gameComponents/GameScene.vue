d7779b019f9a8fd3fed116663529132685b48df6
Jacopostortini <jacostortini@gmail.com>
2021-04-07 10:04:53 +0200

    J

--- src/components/gameComponents/GameScene.vue
+++ src/components/gameComponents/GameScene.vue
@@ -8,7 +8,7 @@ import {config} from "../../constants/constants";
 import GameScene from "../../phaser/GameScene";
 import {gameDimensions} from "../../constants/gameSettings";
 import websocketEvents from "../../constants/websocketEvents";
-import RankingScene from "../../phaser/RankingScene";
+//import RankingScene from "../../phaser/RankingScene";
 
 export default {
   name: "GameScene",
@@ -20,7 +20,7 @@ export default {
       const parent = document.getElementById("game");
       new Phaser.Game(
           config(
-              [new GameScene(this.socket, game), new RankingScene(this.socket, game.settings.pointsToWin)],
+              [new GameScene(this.socket, game)/*, new RankingScene(this.socket, game.settings.pointsToWin)*/],
               parent,
               gameDimensions.width,
               gameDimensions.height,
