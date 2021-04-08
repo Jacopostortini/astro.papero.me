0c2e8fa8c9ce23b28c278472902354624cfbf844
Jacopostortini <jacostortini@gmail.com>
2021-04-08 14:03:09 +0200

    J

--- src/phaser/maps.js
+++ src/phaser/maps.js
@@ -2,37 +2,85 @@ const maps = [
     [
         {
             texture: "block1",
+            killable: true,
             position: {
-                x: 128+32,
+                x: 160,
                 y: 32
             }
         },
         {
             texture: "block1",
+            killable: true,
             position: {
-                x: 128+32,
-                y: 64+32
+                x: 160,
+                y: 96
             }
         },
         {
             texture: "block1",
+            killable: true,
             position: {
-                x: 128+32,
-                y: 128+32
+                x: 160,
+                y: 160
             }
         },
         {
             texture: "block1",
+            killable: true,
             position: {
-                x: 64+32,
-                y: 128+32
+                x: 96,
+                y: 160
             }
         },
         {
             texture: "block1",
+            killable: true,
             position: {
-                x: 0,
-                y: 128+32
+                x: 32,
+                y: 160
+            }
+        },
+        
+        
+
+        {
+            texture: "block1",
+            killable: false,
+            position: {
+                x: 160,
+                y: 568
+            }
+        },
+        {
+            texture: "block1",
+            killable: false,
+            position: {
+                x: 160,
+                y: 504
+            }
+        },
+        {
+            texture: "block1",
+            killable: false,
+            position: {
+                x: 160,
+                y: 440
+            }
+        },
+        {
+            texture: "block1",
+            killable: false,
+            position: {
+                x: 96,
+                y: 440
+            }
+        },
+        {
+            texture: "block1",
+            killable: false,
+            position: {
+                x: 32,
+                y: 440
             }
         }
     ],
@@ -40,13 +88,22 @@ const maps = [
 
 export default (ctx) => {
     const map = maps[0];
-    ctx.mapObjetcs = ctx.physics.add.staticGroup();
+    ctx.killableMapObjects = ctx.physics.add.staticGroup();
+    ctx.notKillableMapObjects = ctx.physics.add.staticGroup();
     map.forEach(obj => {
-       ctx.mapObjetcs.create(obj.position.x, obj.position.y, obj.texture);
-       ctx.physics.add.collider(ctx.mapObjetcs, ctx.ships);
-        ctx.physics.add.collider(ctx.mapObjetcs, ctx.bullets, (obj, bullet) => {
-            obj.destroy();
-            bullet.destroy();
-        });
+       if(obj.killable){
+           ctx.killableMapObjects.create(obj.position.x, obj.position.y, obj.texture);
+       } else {
+           ctx.notKillableMapObjects.create(obj.position.x, obj.position.y, obj.texture);
+       }
+    });
+    ctx.physics.add.collider(ctx.killableMapObjects, ctx.ships);
+    ctx.physics.add.collider(ctx.notKillableMapObjects, ctx.ships);
+    ctx.physics.add.overlap(ctx.killableMapObjects, ctx.bullets, (obj, bullet) => {
+        obj.destroy();
+        bullet.destroy();
+    });
+    ctx.physics.add.overlap(ctx.notKillableMapObjects, ctx.bullets, (obj, bullet) => {
+        bullet.destroy();
     });
 };
