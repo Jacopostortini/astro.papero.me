efe9de07d30573df3db400bb47d7fc5e1ac2df4e
Jacopostortini <jacostortini@gmail.com>
2021-04-08 14:31:36 +0200

    J

---
+++ src/constants/mapConstants.js
@@ -0,0 +1,21 @@
+import {gameDimensions} from "./gameSettings";
+
+const getStartingPrisons = () => {
+    let objs = [];
+    for(let i = 0; i < 4; i++){
+        for(let j = 0; j < 5; j++) {
+            objs.push({
+                texture: "block1",
+                killable: true,
+                position: {
+                    x: ( 160 - (j>2 ? 64*(j-2) : 0) ) * (1-i%2)   +   (gameDimensions.width-160 + (j>2 ? 64*(j-2) : 0) ) * (1-i%2),
+                    y: i<2 ? ( 32 + Math.min(j*64, 128) ) :  (gameDimensions.height-32 - Math.min(j*64, 128))
+                }
+            });
+        }
+    }
+    return objs;
+
+}
+
+export { getStartingPrisons };
