57d2216a777a8bb60c4342e02c493e35698d31e9
Jacopostortini <jacostortini@gmail.com>
2021-04-08 13:53:02 +0200

    J

--- src/phaser/maps.js
+++ src/phaser/maps.js
@@ -3,36 +3,36 @@ const maps = [
         {
             texture: "block1",
             position: {
-                x: 128,
-                y: 0
+                x: 128+32,
+                y: 32
             }
         },
         {
             texture: "block1",
             position: {
-                x: 128,
-                y: 64
+                x: 128+32,
+                y: 64+32
             }
         },
         {
             texture: "block1",
             position: {
-                x: 128,
-                y: 128
+                x: 128+32,
+                y: 128+32
             }
         },
         {
             texture: "block1",
             position: {
-                x: 64,
-                y: 128
+                x: 64+32,
+                y: 128+32
             }
         },
         {
             texture: "block1",
             position: {
                 x: 0,
-                y: 128
+                y: 128+32
             }
         }
     ],
@@ -42,7 +42,7 @@ export default (ctx) => {
     const map = maps[0];
     ctx.mapObjetcs = ctx.physics.add.staticGroup();
     map.forEach(obj => {
-       ctx.mapObjetcs.create(obj.position.x, obj.position.y, obj.texture).setOrigin(0, 0);
+       ctx.mapObjetcs.create(obj.position.x, obj.position.y, obj.texture);
        ctx.physics.add.collider(ctx.mapObjetcs, ctx.ships);
         ctx.physics.add.collider(ctx.mapObjetcs, ctx.bullets, (obj, bullet) => {
             obj.destroy();
