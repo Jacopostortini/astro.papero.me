921c35cb1cd132fdb0120dd755deaf9e05805caf
Jacopostortini <jacostortini@gmail.com>
2021-04-15 13:12:03 +0200

    J

--- src/phaser/GameScene.js
+++ src/phaser/GameScene.js
@@ -235,8 +235,10 @@ export default class GameScene extends Phaser.Scene {
         laser.setAngle(data.angle);
         laser.setPosition(laser.x+maxLength/2*Math.cos(data.angle*Math.PI/180), laser.y+maxLength/2*Math.sin(data.angle*Math.PI/180));
         laser.setCollidesWith([]);
+        this.players[data.localId].ship.setActive(false);
         setTimeout(()=>{
             laser.destroy();
+            this.players[data.localId].ship.setActive(true);
         }, 200);
     }
 
