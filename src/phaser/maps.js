f8b45be828bf114bc2650685028fa4c29bfcbd02
Jacopostortini <jacostortini@gmail.com>
2021-04-15 18:33:13 +0200

    J

--- src/phaser/maps.js
+++ src/phaser/maps.js
@@ -1,11 +1,11 @@
-import {getCenteredCross, getCenteredSquare, getMovingSlalom, getStartingPrisons, getMovingVertical, getMixedOrizontalLine} from "../constants/mapConstants";
+import {getCenteredCross, getCenteredSquare, getMovingSlalom, getStartingPrisons, /*getMovingVertical,*/ getMixedOrizontalLine} from "../constants/mapConstants";
 import {getBodyFromCollision} from "./scene";
 
 const maps = [
     [...getStartingPrisons(), ...getCenteredSquare(5)],
     [...getMovingSlalom(4)],
     [...getCenteredCross(500, 100), ...getCenteredSquare(3)],
-    [...getMovingVertical(3), ...getMixedOrizontalLine()]
+    [/*...getMovingVertical(3), */...getMixedOrizontalLine()]
 ];
 
 export default (ctx) => {
@@ -13,17 +13,14 @@ export default (ctx) => {
     map.forEach(obj => {
         const o = ctx.matter.add.image(obj.position.x, obj.position.y, obj.texture, null, {...ctx.defaultImageOptions});
         o.setCollisionCategory(ctx.mapObjectCategory);
-        if(obj.velocity) o.setVelocity(obj.velocity.x, obj.velocity.y);
-        if(obj.bounce) o.setBounce(obj.bounce);
-        o.setCollidesWith([1, ctx.shipsCategory, ctx.bulletsCategory]);
+        /*if(obj.velocity) o.setVelocity(obj.velocity.x, obj.velocity.y);
+        if(obj.bounce) o.setBounce(obj.bounce);*/
+        o.setCollidesWith([1, ctx.shipsCategory, ctx.bulletsCategory, ctx.powerUpsCategory]);
         o.setStatic(true);
-        ctx.matter.body.setInertia(o.body, Infinity);
-        //ctx.matter.body.setMass(o.body, Infinity);
         o.setOnCollide(collision => {
             const body = getBodyFromCollision(o.body.id, collision);
             if(body.gameObject) {
                 if(obj.killable && body.collisionFilter.category === ctx.bulletsCategory){
-                    console.log("destroying:", o, "after collision:", collision);
                     o.destroy();
                 }
             }
