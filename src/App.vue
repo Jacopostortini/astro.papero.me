a04021805d9dc0c7a533eca90740d8a9f87f4593
matteo <matte.rubbiani@gmail.com>
2021-05-06 09:45:15 +0200

    J

--- src/App.vue
+++ src/App.vue
@@ -60,7 +60,7 @@ button{
 #app {
   width: 100%;
   height: 100%;
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: "Comic Sans MS", sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
